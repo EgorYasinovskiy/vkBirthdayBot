ca8bacf4d88c09c0cc140bc63b5fe1739720e32f
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2019-11-19 22:59:12 +0300

    Added some functions. Sending and getting greeting messages. Chiking for birthday everyday in 00:00.

--- VKBysLogic.cs
+++ VKBysLogic.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading;
 using VkNet;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
 using VkNet.Enums.Filters;
 using VkNet.Categories;
+using System.Threading.Tasks;
 
 namespace VkBirthdayApp
 {
@@ -14,14 +15,19 @@ namespace VkBirthdayApp
         private string _password;
         private string _login;
         private ulong _appKey;
-        
+        private List<User> _friendList;
 
+        
+        
         public string Password { get => _password; set => _password = value; }
         public string Login { get => _login; set => _login = value; }
         public ulong AppKey { get => _appKey; set => _appKey = value; }
+        public List<User> FriendList { get => _friendList; set => _friendList = value; }
         
         public VkApi api = new VkApi();
-
+        /// <summary>
+        /// Makes auth in vk.com using AppID, Login and Password
+        /// </summary>
         public void Auth()
         {
             api.Authorize(new VkNet.Model.ApiAuthParams
@@ -32,6 +38,10 @@ namespace VkBirthdayApp
                 Settings = VkNet.Enums.Filters.Settings.All
             });
         }
+        /// <summary>
+        /// Parsing your friendlist from vk, takes it's birthdate ,firstname and ID to send they greetings messages
+        /// </summary>
+        /// <returns> FriendLsit with birthdate, firstname, ID</returns>
         public List<User> GetFriendList()
         {
             var friends = api.Friends.Get(new FriendsGetParams { });
@@ -48,15 +58,70 @@ namespace VkBirthdayApp
                     User friend = new User();
                     friend.BirthDate = item.BirthDate.Substring(0, 5);
                     friend.FirstName = item.FirstName;
+                    friend.Id = item.Id;
                     FriendList.Add(friend);
                 }
             }
             return FriendList;
-
-            
         }
-        
-        
+        /// <summary>
+        /// Refreshing friendlist for know if user added new friends or deleted someone
+        /// </summary>
+        public void RefreshFriendList()
+        {
+            FriendList = GetFriendList(); 
+        }
+        /// <summary>
+        /// Forming the greetings messages using your friend name
+        /// </summary>
+        /// <param name="friend">Object of User class</param>
+        /// <returns>Greeting message</returns>
+        private string GetGreetingMessage(User friend)
+        {
+            if(friend.Sex==VkNet.Enums.Sex.Female)
+            {
+                return string.Format("Дорогая {0}, поздравляю тебя с днем рождения," +
+                    " желаю тебе всегда улыбаться и быть счастливой." +
+                    "Хочу чтобы в твоей жизни были только хорошие моменты, а" +
+                    " сегодняшний день ты запомнила навсегла как один из лучших твоих дней!!!",
+                    friend.FirstName);
+            }
+            else
+            {
+                return string.Format("С днем рождени,{0}!!!Хочу пожелать тебе всео наилучшего," +
+                    " побольше счастья, здоровья и конечно же денег. Будь счастлив в этот день!", friend.FirstName);
+            }
+        }
+        /// <summary>
+        /// Sends your friend the greeting message
+        /// </summary>
+        /// <param name="friend">Object of User class, who should get the greeting message </param>
+        public void SendGreetings(User friend)
+        { 
+            api.Messages.Send(new MessagesSendParams
+            {
+                UserId = friend.Id,
+                RandomId = new Random().Next(),
+                Message = GetGreetingMessage(friend)
+            });
+        }
+        /// <summary>
+        /// Everyday checking if someone has birthday today. If someone has - sends greeting, wait for next day;
+        /// </summary>
+        public async void  CheckBirthday()
+        {
+            RefreshFriendList();
+            foreach(var friend in FriendList)
+            {
+                if (friend.BirthDate == DateTime.Now.ToShortDateString().Substring(0, 5))
+                {
+                    SendGreetings(friend);
+                }
+            }
+            var Time = (DateTime.Now.AddDays(1) - DateTime.Now).TotalMilliseconds;
+            await Task.Run(() => Thread.Sleep(Convert.ToInt32(Time)));
+        }
         
     }
 }
+
