1d64ab11af29a5c7ef6f503c06b9409e4fbe8cf3
Egor Yasinovskiy <yasinovskiy.egor1@gmail.com>
2019-11-20 06:45:03 +0300

    Project ended. All was tested. Application works correct

--- Program.cs
+++ Program.cs
@@ -1,9 +1,127 @@
 ﻿using System;
-using VkNet;
+using System.Collections.Generic;
 
 namespace VkBirthdayApp
 {
     class Program
     {
+        static bool IsStarted = false;
+        static bool IsAuthed = false;
+        static VKBysLogic Vk;
+        static Dictionary<string, Action> Commands = new Dictionary<string, Action>
+        {
+            { "setuser" ,SetUser},
+            { "auth",Auth },
+            { "start",Start},
+            { "stop",Stop },
+            { "exit",(()=>Environment.Exit(0))},
+            { "cls",(()=>Console.Clear())},
+            { "help",Help }
+        };
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string command;
+                command = Console.ReadLine();
+                if (Commands.ContainsKey(command))
+                {
+                    Console.WriteLine("\n");
+                    Commands[command]();
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда: \"{0}\"\nНапишите\"help\" Для просмотра команд\n",command);
+                }
+            }
+
+
+
+        }
+        public static void  Help()
+        {
+            string commands="";
+            foreach(var command in Commands)
+            {
+                commands += string.Format("{0}, ", command.Key);
+            }
+            Console.WriteLine(commands);
+        }
+        public static void SetUser()
+        {
+            string Login;
+            string Pass;
+            
+            Console.WriteLine("> Введите логин \n");
+            while(string.IsNullOrEmpty(Login = Console.ReadLine()))
+            {
+                Console.WriteLine(">Введите корректный логин \n");
+            }
+            Console.WriteLine(">Введите пароль\n");
+            while (string.IsNullOrEmpty(Pass=Console.ReadLine()))
+            {
+                Console.WriteLine(">Введите корректный пароль\n");
+            }
+            Vk = new VKBysLogic(Pass, Login);
+            Auth();
+            
+
+
+
+        }
+        public static void Auth()
+        {
+            if (Vk == null)
+            {
+                Console.WriteLine("Сначала войдите используя команду  \"setuser\"\n");
+            }
+            else
+            {
+                try
+                {
+                    Vk.Auth();
+                    IsAuthed = true;
+                    Console.WriteLine("Успешный вход\n");
+                }
+                catch
+                {
+                    Console.WriteLine("Что-то пошло не так. Проверьте ваш логин и пароль, а так же подключение к интернету\n");
+                }
+            }
+        }
+        public static void Start()
+        {
+            if (Vk == null)
+            {
+                Console.WriteLine("Сначала войдите используя команду \"setuser\"\n");
+            }
+            else if(!IsAuthed)
+            {
+                Console.WriteLine("Сначала авторизуйтесь используя команду \"auth\"\n");
+            }
+            else
+            {
+                IsStarted = true;
+                
+                Vk.CheckBirthday(IsStarted);
+            }
+                
+            
+        }
+        public static void Stop()
+        {
+            if (IsStarted)
+            {
+                IsStarted = false;
+                Console.WriteLine("Проверка на дни рождения остановлена\n");
+            }
+            else
+            {
+                Console.WriteLine("Проверка не была запущена!!!\n");
+            }
+        }
+        
+
     }
 }
--- VKBysLogic.cs
+++ VKBysLogic.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
-using VkNet;
+using VkNet.Abstractions;
 using VkNet.Model;
+using VkNet.AudioBypassService.Extensions;
 using VkNet.Model.RequestParams;
 using VkNet.Enums.Filters;
-using VkNet.Categories;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet;
 
 namespace VkBirthdayApp
 {
@@ -14,65 +15,56 @@ namespace VkBirthdayApp
     {
         private string _password;
         private string _login;
-        private ulong _appKey;
-        private List<User> _friendList;
-        
-        private VkApi api = new VkApi();
+        private VkNet.Utils.VkCollection<User> _friendList;
 
+        static ServiceCollection services = new ServiceCollection();
+        private static IVkApi api;
         /// <summary>
-        /// Makes auth in vk.com using AppID, Login and Password
+        /// Заходит в вк используя логин и пароль
         /// </summary>
         public void Auth()
         {
-            api.Authorize(new VkNet.Model.ApiAuthParams
+            services.AddAudioBypass();
+            api = new VkApi(services);
+            Console.WriteLine("Авторизация...\n\n");
+            try
             {
-                Login = _login,
-                Password = _password,
-                ApplicationId = _appKey,
-                Settings = VkNet.Enums.Filters.Settings.All
-            });
-        }
-        /// <summary>
-        /// Parsing your friendlist from vk, takes it's birthdate ,firstname and ID to send they greetings messages
-        /// </summary>
-        /// <returns> FriendLsit with birthdate, firstname, ID</returns>
-        public List<User> GetFriendList()
-        {
-            var friends = api.Friends.Get(new FriendsGetParams { });
-            List<User> FriendList = new List<User>();
-            int i = 0;
-            foreach(var item in friends)
-            {
-                if(item.BirthDate == null)
+                api.Authorize(new VkNet.Model.ApiAuthParams
                 {
-                    continue;
-                }
-                else
-                { 
-                    User friend = new User();
-                    friend.BirthDate = item.BirthDate.Substring(0, 5);
-                    friend.FirstName = item.FirstName;
-                    friend.Id = item.Id;
-                    FriendList.Add(friend);
-                }
+                    Login = _login,
+                    Password = _password,
+                    TwoFactorAuthorization = () =>
+                    {
+                        Console.WriteLine(" > Код двухфакторной аутентификации:");
+                        return Console.ReadLine();
+                    }
+                });
             }
-            return FriendList;
+            catch
+            {
+                Console.WriteLine("Ошибка. Попытайтесь снова");
+            }
         }
         /// <summary>
-        /// Refreshing friendlist for know if user added new friends or deleted someone
+        /// Получение списка друзей и их имен, даты рождения, айди
         /// </summary>
-        public void RefreshFriendList()
+        /// <returns> Список user</returns>
+        public VkNet.Utils.VkCollection<User> GetFriendList()
         {
-           _friendList = GetFriendList(); 
+            Console.WriteLine("Getting friendlist\n\n");
+
+            return api.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams { UserId = api.UserId, Fields = ProfileFields.BirthDate | ProfileFields.FirstName | ProfileFields.Sex });
+
+
         }
         /// <summary>
-        /// Forming the greetings messages using your friend name
+        /// Формирует сообщение перед отправкой на основе пола
         /// </summary>
-        /// <param name="friend">Object of User class</param>
-        /// <returns>Greeting message</returns>
+        /// <param name="friend">Экземпляр класса User для получения пола и имени </param>
+        /// <returns>Поздравительное сообщение</returns>
         private string GetGreetingMessage(User friend)
         {
-            if(friend.Sex==VkNet.Enums.Sex.Female)
+            if (friend.Sex == VkNet.Enums.Sex.Female)
             {
                 return string.Format("Дорогая {0}, поздравляю тебя с днем рождения," +
                     " желаю тебе всегда улыбаться и быть счастливой." +
@@ -82,47 +74,74 @@ namespace VkBirthdayApp
             }
             else
             {
-                return string.Format("С днем рождени,{0}!!!Хочу пожелать тебе всео наилучшего," +
+                return string.Format("С днем рождения,{0}!!!Хочу пожелать тебе всео наилучшего," +
                     " побольше счастья, здоровья и конечно же денег. Будь счастлив в этот день!", friend.FirstName);
             }
         }
         /// <summary>
-        /// Sends your friend the greeting message
+        /// Отправляет сообщение именнинику
         /// </summary>
-        /// <param name="friend">Object of User class, who should get the greeting message </param>
+        /// <param name="friend">Экземпляр класса User для получения id, перед отправкой сообщения  </param>
         public void SendGreetings(User friend)
-        { 
+        {
             api.Messages.Send(new MessagesSendParams
             {
                 UserId = friend.Id,
                 RandomId = new Random().Next(),
                 Message = GetGreetingMessage(friend)
+
             });
+            Console.WriteLine("Сообщение отправлено\n\n");
         }
         /// <summary>
-        /// Everyday checking if someone has birthday today. If someone has - sends greeting, wait for next day;
+        /// Каждый день проверяет, наступил ли у кого день рождения
+        /// Если наступил - отправляет поздравительное сообщение
         /// </summary>
-        public async void  CheckBirthday()
+        public async void CheckBirthday(bool IsStarted)
         {
-            RefreshFriendList();
-            foreach(var friend in _friendList)
+            while (IsStarted)
             {
-                if (friend.BirthDate == DateTime.Now.ToShortDateString().Substring(0, 5))
+                Console.WriteLine("Начало проверки\n\n");
+                _friendList = GetFriendList();
+                Console.WriteLine(_friendList.Count);
+                Console.WriteLine(_friendList);
+                int greeted = 0;
+                foreach (var friend in _friendList)
                 {
-                    SendGreetings(friend);
+                    if (friend.BirthDate == DateTime.Now.ToShortDateString().Substring(0, 5))
+                    {
+                        SendGreetings(friend);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Друг {0} поздравлен!", friend.FirstName);
+                        Console.ResetColor();
+                        greeted++;
+                    }
                 }
+                if (greeted == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Никто сегодня не празднует ДР :(");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Сегодня поздравили {0} человек(а)", greeted);
+                    Console.ResetColor();
+                }
+
+                var Time = (DateTime.Now.AddDays(1).Date - DateTime.Now).TotalMilliseconds;
+                
+                Console.WriteLine("Жду до следующего дня {0} часов", Time/(1000*60*60));
+                await Task.Run(() => Thread.Sleep(Convert.ToInt32(Time)));
             }
-            var Time = (DateTime.Now.AddDays(1) - DateTime.Now).TotalMilliseconds;
-            await Task.Run(() => Thread.Sleep(Convert.ToInt32(Time)));
         }
 
-        public VKBysLogic(string Password,string Login, ulong AppKey)
+        public VKBysLogic(string Password, string Login)
         {
             _password = Password;
             _login = Login;
-            _appKey = AppKey;
         }
-        
     }
 }
 
